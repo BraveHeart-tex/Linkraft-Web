3b74e8ec18020f2d20a15d961ab0751598ae1a70
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2025-04-29 12:32:30 +0300

    feat: add sidebar state and cookies integration to keep the sidebar state between page refresh

--- src/app/(appHome)/layout.tsx
+++ src/app/(appHome)/layout.tsx
@@ -3,15 +3,19 @@ import AppSidebarHeader from '@/components/AppSidebarHeader';
 import ModalHost from '@/components/Modalhost';
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 import SocketProvider from '@/context/SocketProvider';
+import { cookies } from 'next/headers';
 
-const AppLevelLayout = ({
+const AppLevelLayout = async ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
+  const cookieStore = await cookies();
+  const defaultOpen = cookieStore.get('sidebar_state')?.value === 'true';
+
   return (
     <SocketProvider>
-      <SidebarProvider>
+      <SidebarProvider defaultOpen={defaultOpen}>
         <AppSidebar />
         <SidebarInset>
           <AppSidebarHeader />

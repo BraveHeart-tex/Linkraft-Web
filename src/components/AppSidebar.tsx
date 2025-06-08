c3f4cb97ab3c07dfcd94c1fa3d8d0332ce421b1e
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2025-06-08 16:56:47 +0300

    feat(AppSidebar): integrate current user prefetching in sidebar

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -10,6 +10,7 @@ import {
   SidebarMenu,
   SidebarMenuItem,
 } from '@/components/ui/Sidebar';
+import { getCurrentUser } from '@/features/auth/auth.server';
 import { getCollections } from '@/features/collections/collection.server';
 import SimpleTreeview from '@/features/collections/TreeView/CollectionTreeView';
 import UserMenu from '@/features/users/UserMenu';
@@ -24,44 +25,50 @@ import { PlusIcon } from 'lucide-react';
 const AppSidebar = async () => {
   const queryClient = new QueryClient();
 
-  await queryClient.prefetchQuery({
-    queryKey: QUERY_KEYS.collections.list(),
-    queryFn: getCollections,
-  });
+  await Promise.all([
+    queryClient.prefetchQuery({
+      queryKey: QUERY_KEYS.collections.list(),
+      queryFn: getCollections,
+    }),
+    queryClient.prefetchQuery({
+      queryKey: QUERY_KEYS.auth.currentUser(),
+      queryFn: () => getCurrentUser().then((result) => result.user),
+    }),
+  ]);
 
   return (
-    <Sidebar>
-      <SidebarContent className="gap-0">
-        <SidebarGroup className="py-2">
-          <SidebarGroupContent>
-            <SidebarMenu className="gap-0">
-              <div className="flex items-center w-full justify-between gap-2 my-2">
-                <SidebarMenuItem className="flex-1">
-                  <UserMenu />
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Sidebar>
+        <SidebarContent className="gap-0">
+          <SidebarGroup className="py-2">
+            <SidebarGroupContent>
+              <SidebarMenu className="gap-0">
+                <div className="flex items-center w-full justify-between gap-2 my-2">
+                  <SidebarMenuItem className="flex-1">
+                    <UserMenu />
+                  </SidebarMenuItem>
+                  <Button size="icon" variant="ghost">
+                    <PlusIcon />
+                  </Button>
+                </div>
+                <SidebarMenuItem>
+                  <SidebarSearchButton />
                 </SidebarMenuItem>
-                <Button size="icon" variant="ghost">
-                  <PlusIcon />
-                </Button>
-              </div>
-              <SidebarMenuItem>
-                <SidebarSearchButton />
-              </SidebarMenuItem>
-              <AppMenu />
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-        <SidebarGroup className="p-0">
-          <SidebarGroupLabel className="px-4 py-1">
-            Collections
-          </SidebarGroupLabel>
-          <SidebarGroupContent>
-            <HydrationBoundary state={dehydrate(queryClient)}>
+                <AppMenu />
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+          <SidebarGroup className="p-0">
+            <SidebarGroupLabel className="px-4 py-1">
+              Collections
+            </SidebarGroupLabel>
+            <SidebarGroupContent>
               <SimpleTreeview />
-            </HydrationBoundary>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent>
-    </Sidebar>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+      </Sidebar>
+    </HydrationBoundary>
   );
 };
 
